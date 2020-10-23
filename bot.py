e7b5d6c50d641aa41b853cdeaed5c2f1c4e5bd38
Valentin Guniakov <radioval87@yandex.ru>
2020-10-23 20:18:58 +0300

    trying to restrict a newcomer

--- bot.py
+++ bot.py
@@ -17,12 +17,10 @@ logging.info("RV87_test_bot launched.")
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
+WELCOME_MSG = ("Добро пожаловать, но у нас закрытое сообщество, поэтому " 
+               "тэгни @ того, кто тебя сюда пригласил. У тебя есть 1 минута")
+
 bot = telegram.Bot(TELEGRAM_TOKEN)
-
-
-def msg(msg):
-    bot.sendMessage(chat_id=chat_id, text=msg)
-    logging.info("Sent message: " + msg)
 
 def get_updates():
     updates = bot.getUpdates()
@@ -49,10 +47,16 @@ def get_new_members(last_update_id):
     return new_members
 
 def get_chat_id(last_update_id):
-    for update in bot.getUpdates(offset=last_update_id, timeout=5):
+    chat_id = None
+    for update in bot.getUpdates():
         if last_update_id < update.update_id:
             chat_id = update.message.chat_id
     return chat_id
+
+# def msg(msg):
+#     chat_id = get_chat_id(last_update_id)
+#     bot.sendMessage(chat_id=chat_id, text=msg)
+#     logging.info("Sent message: " + msg)
 
 def authentication():
     pass
@@ -60,69 +64,80 @@ def authentication():
 def main():
     # global last_update_id
     # global chat_id
-    updates = get_updates()
-    last_update_id = get_last_update_id(updates)
-    new_members = get_new_members(last_update_id) or None
-    chat_id = get_chat_id(last_update_id)
-    if new_members:
-
-        if(user):
-            try:
-                username = update.message.new_chat_members[0].username
-                first_name = update.message.new_chat_members[0].first_name
-                last_name = update.message.new_chat_members[0].last_name
-                group = update.message.chat.title
-                msg('Welcome in the group ' + group + ', ' + first_name + ' ' + last_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                pass
-            except:
-                try:
-                    username = update.message.new_chat_members[0].username
-                    first_name = update.message.new_chat_members[0].first_name
-                    group = update.message.chat.title
-                    msg('Welcome in the group ' + group + ', ' + first_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                    pass
-                except:
-                    try:
-                        username = update.message.new_chat_members[0].username
-                        last_name = update.message.new_chat_members[0].first_name
-                        group = update.message.chat.title
-                        msg('Welcome in the group ' + group + ', ' + last_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                        pass
-                    except:
-                        try:
-                            first_name = update.message.new_chat_members[0].first_name
-                            last_name = update.message.new_chat_members[0].last_name
-                            group = update.message.chat.title
-                            msg('Welcome in the group ' + group + ', ' + first_name + ' ' + last_name + '!\nJust ask a question.\nBbut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                            pass
-                        except:
-                            try:
-                                first_name = update.message.new_chat_members[0].first_name
-                                group = update.message.chat.title
-                                msg('Welcome in the group ' + group + ', ' + first_name + '!\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                                pass
-                            except:
-                                username = update.message.new_chat_members[0].username
-                                group = update.message.chat.title
-                                msg('Welcome in the group ' + group + ', ' + ' @' + username + '!\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
-                                pass
-                            pass
-                        pass
-                    pass
-                pass
-        else:
-            msg('Not a valid language!')
-        pass
-    else:
-        pass
-
-    last_update_id = update.update_id
+    while True:
+        updates = get_updates()
+        last_update_id = get_last_update_id(updates)
+        new_members = get_new_members(last_update_id) or None
+        chat_id = get_chat_id(last_update_id)
+        if new_members:
+            for new_member in new_members:
+                print(new_member[0])
+                bot.restrictChatMember(
+                    chat_id=chat_id, 
+                    user_id=new_member[0].id, 
+                    permissions=telegram.ChatPermissions(can_send_messages = False)
+                )
+                bot.sendMessage(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text=WELCOME_MSG)
 
 if __name__ == '__main__':
     db.init_db()
     while True:
         main()
         time.sleep(1)
+
+    #     if(user):
+    #         try:
+    #             username = update.message.new_chat_members[0].username
+    #             first_name = update.message.new_chat_members[0].first_name
+    #             last_name = update.message.new_chat_members[0].last_name
+    #             group = update.message.chat.title
+    #             msg('Welcome in the group ' + group + ', ' + first_name + ' ' + last_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #             pass
+    #         except:
+    #             try:
+    #                 username = update.message.new_chat_members[0].username
+    #                 first_name = update.message.new_chat_members[0].first_name
+    #                 group = update.message.chat.title
+    #                 msg('Welcome in the group ' + group + ', ' + first_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #                 pass
+    #             except:
+    #                 try:
+    #                     username = update.message.new_chat_members[0].username
+    #                     last_name = update.message.new_chat_members[0].first_name
+    #                     group = update.message.chat.title
+    #                     msg('Welcome in the group ' + group + ', ' + last_name + '! - @' + username + '\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #                     pass
+    #                 except:
+    #                     try:
+    #                         first_name = update.message.new_chat_members[0].first_name
+    #                         last_name = update.message.new_chat_members[0].last_name
+    #                         group = update.message.chat.title
+    #                         msg('Welcome in the group ' + group + ', ' + first_name + ' ' + last_name + '!\nJust ask a question.\nBbut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #                         pass
+    #                     except:
+    #                         try:
+    #                             first_name = update.message.new_chat_members[0].first_name
+    #                             group = update.message.chat.title
+    #                             msg('Welcome in the group ' + group + ', ' + first_name + '!\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #                             pass
+    #                         except:
+    #                             username = update.message.new_chat_members[0].username
+    #                             group = update.message.chat.title
+    #                             msg('Welcome in the group ' + group + ', ' + ' @' + username + '!\nJust ask a question.\nBut first check me out! @WelcomeInDaHoodBot for the FAQ')
+    #                             pass
+    #                         pass
+    #                     pass
+    #                 pass
+    #             pass
+    #     else:
+    #         msg('Not a valid language!')
+    #     pass
+    # else:
+    # #     pass
+
+    # last_update_id = update.update_id
+
+
 
 
 
