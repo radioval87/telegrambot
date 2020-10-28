175d492d5f2f3fb3691ff798c8bf65fd58b08f1d
Valentin Guniakov <radioval87@yandex.ru>
2020-10-28 22:38:49 +0300

    Plus SQLalchemy, plus interface

--- database/database.py
+++ database/database.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
-from sqlalchemy import create_engine, Column, Integer, Boolean, String, VARCHAR, DateTime
+
+from sqlalchemy import (VARCHAR, Boolean, Column, DateTime, Integer, String,
+                        create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -8,17 +10,6 @@ engine = create_engine('sqlite:///database/data.db', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
-# class Auth(Base):
-#     __tablename__ = 'auth'
-#     id = Column('pk', Integer, primary_key=True, nullable=False)
-#     tg_id = Column('tg_id', Integer, unique=True, nullable=False)
-#     auth = Column('auth', Boolean, default=0, nullable=False)
-#     active = Column('active', Boolean, default=1, nullable=False)
-#     banned = Column('banned', Boolean, default=0, nullable=False)
-
-
-#     def __str__(self):
-#         return f'User with {self.tg_id} telegram id'
 
 class User(Base):
     __tablename__ = 'users'
@@ -44,14 +35,14 @@ def create(model, **kwargs):
     print(model)
     print(kwargs)
     session = Session()
-    # try:
-    instance = model(**kwargs)
-    session.add(instance)
-    session.commit()
-    # except:
-    #     session.rollback()
-    # finally:
-    #     session.close()
+    try:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+    except:
+        session.rollback()
+    finally:
+        session.close()
 
 def get(model, **kwargs):
     session = Session()
@@ -63,11 +54,6 @@ def get(model, **kwargs):
     finally:
         session.close()
 
-
-
-# 1	111	username_RV87_test_bot	jopa			2020-10-28 13:56:17				0	
-# 3	70635837	radioval87	Valentin	Gun		2020-10-28 13:56:17				-1001273799823	
-
 # def get_or_create(session, model, **kwargs):
 #     instance = session.query(model).filter_by(**kwargs).first()
 #     if instance:
@@ -77,50 +63,3 @@ def get(model, **kwargs):
 #         session.add(instance)
 #         session.commit()
 #         return instance
-
-# DB_PATH = './database/data.db'
-
-# def init_db():
-#     if os.path.isfile(DB_PATH) and os.path.getsize(DB_PATH) > 100:
-#         with open(DB_PATH, 'r', encoding="ISO-8859-1") as f:
-#             header = f.read(100)
-#             if header.startswith('SQLite format 3'):
-#                 print("SQLite3 database has been detected.")
-#     else:
-#         conn = sqlite3.connect(DB_PATH)
-
-#         sql_create_auth_table = '''
-#             CREATE TABLE IF NOT EXISTS auth
-#                     (id             INTEGER(10)    NOT NULL     PRIMARY KEY,
-#                     tg_id           INTEGER(10)    NOT NULL     UNIQUE,
-#                     auth            BOOLEAN        NOT NULL     DEFAULT 0,
-#                     active          BOOLEAN        NOT NULL     DEFAULT 1,
-#                     banned          BOOLEAN        NOT NULL     DEFAULT 0);
-#         '''
-        
-#         sql_create_users_table = '''
-#             CREATE TABLE IF NOT EXISTS users
-#                     (id             INTEGER(10)    NOT NULL     PRIMARY KEY,
-#                     tg_id           INTEGER(10)    NOT NULL     UNIQUE,
-#                     is_bot          BOOLEAN        NOT NULL,
-#                     first_name      VARCHAR(50)    NOT NULL,
-#                     last_name       VARCHAR(50),
-#                     username        VARCHAR(50)                 UNIQUE,
-#                     about           VARCHAR(100),
-#                     joined_date     DATETIME       NOT NULL,
-#                     inv_tg_id       INTEGER(10)                 UNIQUE,
-#                     inv_username    VARCHAR(50)                 UNIQUE,
-#                     invites         INTEGER(10)    NOT NULL     UNIQUE,
-#                     enter_chat_id   INTEGER(19)    NOT NULL     UNIQUE,
-#                     active          BOOLEAN        NOT NULL     DEFAULT 1,
-#                     banned          BOOLEAN        NOT NULL     DEFAULT 0),
-#                     FOREIGN KEY(tg_id) REFERENCES auth(tg_id) ON DELETE   CASCADE,
-#                     FOREIGN KEY(author_nickname) REFERENCES users(nickname) ON DELETE   CASCADE);
-#         '''
-        
-
-#         c = conn.cursor()
-#         c.execute(sql_create_users_table)
-#         conn.close()
-#         print('Database has been created')
-
