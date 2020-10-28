f552254789989e64dc0ee689af1e9cf87ba62242
Valentin Guniakov <radioval87@yandex.ru>
2020-10-28 22:36:01 +0300

    Plus SQLalchemy, plus interface

--- bot.py
+++ bot.py
@@ -1,65 +1,130 @@
 import logging
 import os
 import time
+from datetime import datetime
 
 import telegram
 from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Filters, MessageHandler, Updater
 
 import database.database as db
 
-from telegram.ext import Updater
-from telegram.ext import Filters
-from telegram.ext import MessageHandler
-
 load_dotenv()
 
-logger = logging.getLogger("requests").setLevel(logging.WARNING)
+logger = logging.getLogger('requests').setLevel(logging.WARNING)
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-logging.info("RV87_test_bot launched.")
+logging.info('RV87_test_bot launched.')
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-WELCOME_MSG = ("добро пожаловать, но у нас закрытое сообщество, поэтому " 
-               "тэгни @ того, кто тебя сюда пригласил. У тебя есть 1 минута")
-VALIDATION_MSG = "ты ручаешься за него?"
+WELCOME_MSG = ('добро пожаловать, но у нас закрытое сообщество, поэтому ' 
+               'тэгни @ того, кто тебя сюда пригласил. У тебя есть 1 минута')
+VALIDATION_MSG = 'ты ручаешься за'
+VALIDATION_NEGATIVE_MSG = 'Увы, этот участник не мог тебя пригласить, выбери другого (у тебя одна минута)'
+VALIDATION_POSITIVE_MSG = 'куча приветственных слов, напиши #осебе, без этого ты не сможешь общаться в чате'
 
-def msg(chat_id, text):
-    updater.bot.sendMessage(chat_id=chat_id, text=text)
+validation_keyboard = ReplyKeyboardMarkup([['Да', 'Нет']], resize_keyboard=True, one_time_keyboard=True, selective=True)
+remove_keyboard = ReplyKeyboardRemove()
+
+
+def msg(chat_id, text, **kwargs):
+    updater.bot.sendMessage(chat_id=chat_id, text=text, **kwargs)
     logging.info("Sent message: " + text)
 
-def new_member_handler(update, context):
+
+def new_member_start(update, context):
+    dispatcher.remove_handler(handler=dispatcher.handlers[0][0], group=0)
     chat_id=update.message.chat.id
-    # new_member=update.message.new_chat_members[0]
     new_member = update.message.from_user
     msg(chat_id=chat_id, text=(new_member.first_name + ', ' + WELCOME_MSG))
     dispatcher.add_handler(MessageHandler(
         filters=(Filters.user(new_member.id) & 
-                 (Filters.entity('mention') | Filters.entity('text_mention')) & 
-                 Filters.chat(chat_id)), 
+                (Filters.entity('mention') | Filters.entity('text_mention')) & 
+                Filters.chat(chat_id)), 
         callback=new_member_validation))
     dispatcher.add_handler(MessageHandler(
         filters=(Filters.user(new_member.id) & 
-                 Filters.chat(chat_id) & 
-                 (~Filters.entity('mention') | ~Filters.entity('text_mention'))), 
+                Filters.chat(chat_id) & 
+                (~Filters.entity('mention') | ~Filters.entity('text_mention'))), 
         callback=delete_messages))
 
+
 def new_member_validation(update, context):
-    print('reacted')
     chat_id=update.message.chat.id
-    receiver = update.message.text #-------------сделать более интеллектуальным
-    print(receiver)
-    msg(chat_id=chat_id, text=(receiver + ', ' + VALIDATION_MSG))
+    sender = update.message.from_user
+    receiver_username = (update.message.text).strip('@')
+    receiver = db.get(db.User, username=receiver_username) or update.message.entities[0].user
+    if receiver:
+        if receiver.username:
+            msg(chat_id=chat_id, text=('@' + receiver.username + ', ' + VALIDATION_MSG + f' ({sender.first_name}/{sender.username or "без username"})?'), keyboard=validation_keyboard)
+        else:
+            text=f'[{receiver.first_name}](tg://user?id={receiver.id}), {VALIDATION_MSG} \({sender.first_name}/{sender.username or "без username"}\)?'
+            msg(chat_id=chat_id, text=text, reply_markup=validation_keyboard, parse_mode='MarkdownV2')
+        positive_validation_handler = MessageHandler(filters=(Filters.regex('Да') & Filters.chat(chat_id) & Filters.user(receiver.id)), callback=lambda update, context: positive_validation(update, context, sender))
+        negative_validation_handler = MessageHandler(filters=(Filters.regex('Нет') & Filters.chat(chat_id) & Filters.user(receiver.id)), callback=lambda update, context: negative_validation(update, context, sender))
+        dispatcher.add_handler(positive_validation_handler, group=1)
+        dispatcher.add_handler(negative_validation_handler, group=1)
+    else:
+        msg(chat_id=chat_id, text=VALIDATION_NEGATIVE_MSG)
+
+
+def positive_validation(update, context, sender):
+    dispatcher.remove_handler(handler=dispatcher.handlers[1][0], group=1)
+    chat_id = update.message.chat.id
+    inviter = update.message.from_user
+    text=f'{sender.first_name}, {VALIDATION_POSITIVE_MSG}'
+    msg(chat_id=chat_id, text=text, reply_markup=remove_keyboard)
+    dispatcher.remove_handler(handler=dispatcher.handlers[0][1], group=0)
+    dispatcher.remove_handler(handler=dispatcher.handlers[0][0], group=0)
+    dispatcher.add_handler(MessageHandler(
+        filters=(Filters.user(sender.id) & 
+                Filters.regex(r'#осебе') & 
+                Filters.chat(chat_id)), 
+        callback=lambda update, context: add_new_member(update, context, inviter)))
+
+    dispatcher.add_handler(MessageHandler(
+        filters=(Filters.user(sender.id) & 
+                Filters.chat(chat_id) & 
+                (~Filters.regex('#осебе'))), 
+        callback=delete_messages))
+   
     
+def negative_validation(update, context, sender):
+    dispatcher.remove_handler(handler=dispatcher.handlers[1][0], group=1)
+    chat_id = update.message.chat.id
+    until_date = time.time()+86400
+    updater.bot.kick_chat_member(chat_id=chat_id, user_id=sender.id, until_date=until_date)
+
+
+def add_new_member(update, context, inviter):
+    dispatcher.remove_handler(handler=dispatcher.handlers[0][1], group=0)
+    dispatcher.remove_handler(handler=dispatcher.handlers[0][0], group=0)
+    sender = update.message.from_user
+    chat = update.message.chat
+    new_user = {'tg_id': sender.id,
+    'username': sender.username or 'NULL',
+    'first_name': sender.first_name,
+    'last_name': sender.last_name or 'NULL',
+    'about': update.message.text,
+    'joined_date': datetime.utcnow(),
+    'inviter_tg_id': inviter.id,
+    'inviter_username': inviter.username or 'NULL',
+    'inviter_first_name': inviter.first_name,
+    'enter_chat_id': chat.id}
+    print(dispatcher.handlers)
+    db.create(db.User, **new_user)
+
+
 def delete_messages(update, context):
     chat_id=update.message.chat.id
     message_id=update.message.message_id
     updater.bot.deleteMessage(chat_id=chat_id, message_id=message_id) 
-   
+
+
 if __name__ == '__main__':
-    # db.init_db()
     updater = Updater(token=TELEGRAM_TOKEN)
     dispatcher = updater.dispatcher
-    # dispatcher.add_handler(MessageHandler(filters=Filters.status_update.new_chat_members, callback=new_member_handler))
-    dispatcher.add_handler(MessageHandler(filters=(Filters.regex(r'new')), callback=new_member_handler))
+    dispatcher.add_handler(MessageHandler(filters=(Filters.regex(r'new')), callback=new_member_start))
     updater.start_polling()
     updater.idle()
--- database/database.py
+++ database/database.py
@@ -1,37 +1,126 @@
 import os
 import sqlite3
+from sqlalchemy import create_engine, Column, Integer, Boolean, String, VARCHAR, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DB_PATH = './database/data.db'
+engine = create_engine('sqlite:///database/data.db', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
-def init_db():
-    if os.path.isfile(DB_PATH) and os.path.getsize(DB_PATH) > 100:
-        with open(DB_PATH, 'r', encoding="ISO-8859-1") as f:
-            header = f.read(100)
-            if header.startswith('SQLite format 3'):
-                print("SQLite3 database has been detected.")
-    else:
-        conn = sqlite3.connect(DB_PATH)
+# class Auth(Base):
+#     __tablename__ = 'auth'
+#     id = Column('pk', Integer, primary_key=True, nullable=False)
+#     tg_id = Column('tg_id', Integer, unique=True, nullable=False)
+#     auth = Column('auth', Boolean, default=0, nullable=False)
+#     active = Column('active', Boolean, default=1, nullable=False)
+#     banned = Column('banned', Boolean, default=0, nullable=False)
 
-        sql_create_users_table = '''
-            CREATE TABLE IF NOT EXISTS users
-                    (id             INTEGER(10)    NOT NULL     PRIMARY KEY,
-                    tg_id           INTEGER(10)    NOT NULL     UNIQUE,
-                    is_bot          BOOLEAN        NOT NULL,
-                    first_name      VARCHAR(50)    NOT NULL,
-                    last_name       VARCHAR(50),
-                    username        VARCHAR(50)                 UNIQUE,
-                    about           VARCHAR(100),
-                    joined_date     DATETIME       NOT NULL,
-                    inv_tg_id       INTEGER(10)                 UNIQUE,
-                    inv_username    VARCHAR(50)                 UNIQUE,
-                    invites         INTEGER(10)    NOT NULL     UNIQUE,
-                    enter_chat_id   INTEGER(19)    NOT NULL     UNIQUE,
-                    auth            BOOLEAN        NOT NULL     DEFAULT 0,
-                    active          BOOLEAN        NOT NULL     DEFAULT 1,
-                    banned          BOOLEAN        NOT NULL     DEFAULT 0);
-        '''
 
-        c = conn.cursor()
-        c.execute(sql_create_users_table)
-        conn.close()
-        print('Database has been created')
+#     def __str__(self):
+#         return f'User with {self.tg_id} telegram id'
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column('pk', Integer, primary_key=True, nullable=False)
+    tg_id = Column('tg_id', Integer, unique=True, nullable=False)
+    username = Column('username', VARCHAR(50), unique=True)
+    first_name = Column('first_name', VARCHAR(50), nullable=False)
+    last_name = Column('last_name', VARCHAR(50))
+    about = Column('about', VARCHAR(100))
+    joined_date = Column('joined_date', DateTime, nullable=False)
+    inviter_tg_id = Column('inviter_tg_id', Integer)
+    inviter_username = Column('inviter_username', VARCHAR(50))
+    inviter_first_name = Column('inviter_first_name', VARCHAR(50), nullable=False)
+    invites = Column('invites', Integer, default=10)
+    enter_chat_id = Column('enter_chat_id', Integer, nullable=False)
+
+    def __str__(self):
+        return f'User {self.username or self.first_name} with {self.tg_id} telegram id'
+
+Base.metadata.create_all(bind=engine)
+
+def create(model, **kwargs):
+    print(model)
+    print(kwargs)
+    session = Session()
+    # try:
+    instance = model(**kwargs)
+    session.add(instance)
+    session.commit()
+    # except:
+    #     session.rollback()
+    # finally:
+    #     session.close()
+
+def get(model, **kwargs):
+    session = Session()
+    try:
+        instance = session.query(model).filter_by(**kwargs).first()
+        if instance:
+            return instance
+        return None 
+    finally:
+        session.close()
+
+
+
+# 1	111	username_RV87_test_bot	jopa			2020-10-28 13:56:17				0	
+# 3	70635837	radioval87	Valentin	Gun		2020-10-28 13:56:17				-1001273799823	
+
+# def get_or_create(session, model, **kwargs):
+#     instance = session.query(model).filter_by(**kwargs).first()
+#     if instance:
+#         return instance
+#     else:
+#         instance = model(**kwargs)
+#         session.add(instance)
+#         session.commit()
+#         return instance
+
+# DB_PATH = './database/data.db'
+
+# def init_db():
+#     if os.path.isfile(DB_PATH) and os.path.getsize(DB_PATH) > 100:
+#         with open(DB_PATH, 'r', encoding="ISO-8859-1") as f:
+#             header = f.read(100)
+#             if header.startswith('SQLite format 3'):
+#                 print("SQLite3 database has been detected.")
+#     else:
+#         conn = sqlite3.connect(DB_PATH)
+
+#         sql_create_auth_table = '''
+#             CREATE TABLE IF NOT EXISTS auth
+#                     (id             INTEGER(10)    NOT NULL     PRIMARY KEY,
+#                     tg_id           INTEGER(10)    NOT NULL     UNIQUE,
+#                     auth            BOOLEAN        NOT NULL     DEFAULT 0,
+#                     active          BOOLEAN        NOT NULL     DEFAULT 1,
+#                     banned          BOOLEAN        NOT NULL     DEFAULT 0);
+#         '''
+        
+#         sql_create_users_table = '''
+#             CREATE TABLE IF NOT EXISTS users
+#                     (id             INTEGER(10)    NOT NULL     PRIMARY KEY,
+#                     tg_id           INTEGER(10)    NOT NULL     UNIQUE,
+#                     is_bot          BOOLEAN        NOT NULL,
+#                     first_name      VARCHAR(50)    NOT NULL,
+#                     last_name       VARCHAR(50),
+#                     username        VARCHAR(50)                 UNIQUE,
+#                     about           VARCHAR(100),
+#                     joined_date     DATETIME       NOT NULL,
+#                     inv_tg_id       INTEGER(10)                 UNIQUE,
+#                     inv_username    VARCHAR(50)                 UNIQUE,
+#                     invites         INTEGER(10)    NOT NULL     UNIQUE,
+#                     enter_chat_id   INTEGER(19)    NOT NULL     UNIQUE,
+#                     active          BOOLEAN        NOT NULL     DEFAULT 1,
+#                     banned          BOOLEAN        NOT NULL     DEFAULT 0),
+#                     FOREIGN KEY(tg_id) REFERENCES auth(tg_id) ON DELETE   CASCADE,
+#                     FOREIGN KEY(author_nickname) REFERENCES users(nickname) ON DELETE   CASCADE);
+#         '''
+        
+
+#         c = conn.cursor()
+#         c.execute(sql_create_users_table)
+#         conn.close()
+#         print('Database has been created')
+
